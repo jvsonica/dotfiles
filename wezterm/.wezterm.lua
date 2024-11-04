1caf263bcf1b03e20453ab7b079f09cb02e76581
Leonel Araujo <leonel.araujo@addvolt.com>
2024-11-04 23:25:24 +0000

    wezterm: drops title bar and adds some padding

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,19 +1,27 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- Theme
-config.color_scheme = 'Catppuccin Macchiato'
+config.color_scheme = "Catppuccin Macchiato"
 -- config.color_scheme = 'Catppuccin Frappe'
 
 -- Font
-config.font = wezterm.font 'Hack Nerd Font'
+config.font = wezterm.font("Hack Nerd Font")
 config.font_size = 10
 
 -- GUI interface
+config.initial_cols = 150
+config.initial_rows = 30
 config.enable_tab_bar = false
--- config.window_decorations = "NONE"
+config.window_decorations = "NONE"
+config.window_padding = {
+	left = 5,
+	right = 5,
+	top = 5,
+	bottom = 5,
+}
 
 return config
