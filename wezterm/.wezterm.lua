fadf8c8d4acc83cb63d75ac67fd882be94caab1a
Leonel Araujo <leonel.araujo@addvolt.com>
2024-11-17 16:21:17 +0000

    rofi, wezterm, profile: small tweaks

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -18,10 +18,10 @@ config.initial_rows = 30
 config.enable_tab_bar = false
 config.window_decorations = "NONE"
 config.window_padding = {
-	left = 5,
-	right = 5,
+	left = 0,
+	right = 0,
 	top = 5,
-	bottom = 5,
+	bottom = 0,
 }
 
 return config
