51f535fd38b5657ac6c6a44aad0c72838d85e372
Leonel Araujo <leonel.araujo@addvolt.com>
2024-11-17 17:15:42 +0000

    polybar: now using nc-applet and a sys tray, new colors

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -7,8 +7,8 @@ polybar-msg cmd quit
 # killall -q polybar
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar 2>&1 | tee -a /tmp/polybar1.log &
+echo "--" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar -l trace 2>&1 | tee -a /tmp/polybar1.log &
 disown
 
 echo "Bars launched..."
