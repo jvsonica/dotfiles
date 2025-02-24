65236831a8c87b2b858728d2e29c4a71ff9cb1c3
Leonel Araujo <leonel.araujo@addvolt.com>
2025-02-24 21:12:54 +0000

    wezterm: slightly increases font size. drops unused confs

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -6,22 +6,20 @@ local config = wezterm.config_builder()
 
 -- Theme
 config.color_scheme = "Catppuccin Macchiato"
--- config.color_scheme = 'Catppuccin Frappe'
+-- config.color_scheme = "Catppuccin Frappe"
 
 -- Font
 config.font = wezterm.font("Hack Nerd Font")
-config.font_size = 10
+config.font_size = 11.5
 
 -- GUI interface
-config.initial_cols = 150
-config.initial_rows = 30
 config.enable_tab_bar = false
 config.window_decorations = "NONE"
 config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 5,
-	bottom = 0,
+    left = 0,
+    right = 0,
+    top = 5,
+    bottom = 0,
 }
 
 return config
