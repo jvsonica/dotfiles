c4d736a9447082a85368b0b1a1e770317e260670
Leonel Araujo <leonel.araujo@addvolt.com>
2025-04-01 21:48:54 +0100

    wezterm: force window title. default theme now tokyonight. adds april fools font config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,12 +5,20 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- Theme
-config.color_scheme = "Catppuccin Macchiato"
 -- config.color_scheme = "Catppuccin Frappe"
+config.color_scheme = "tokyonight"
 
 -- Font
-config.font = wezterm.font("Hack Nerd Font")
+-- config.font = wezterm.font("Hack Nerd Font")
 config.font_size = 11.5
+
+-- Use Comic Sans on April Fool's
+local april_fools_font = "Comic Shanns Mono Nerd Font"
+local font_exists, _ = pcall(wezterm.font, april_fools_font)
+if os.date("%m-%d") == "04-01" and font_exists then
+    config.font = wezterm.font(april_fools_font)
+    config.font_size = 12
+end
 
 -- GUI interface
 config.enable_tab_bar = false
@@ -21,5 +29,10 @@ config.window_padding = {
     top = 5,
     bottom = 0,
 }
+
+-- Always show "wezterm" as window title
+wezterm.on("format-window-title", function()
+    return "wezterm"
+end)
 
 return config
