b70f890a20fcdb564a89b126ba6384efaac1b713
Leonel Araujo <leonel.araujo@addvolt.com>
2025-08-12 09:32:16 +0100

    polybar: removes agenda module

--- polybar/.config/polybar/scripts/next-appointments.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-START_DATE="today 0:00"
-END_DATE_ARG=$(date -d "+3 days" +%Y-%m-%d)
-
-/usr/bin/gcalcli agenda --nostarted --tsv "$START_DATE" "$END_DATE_ARG" |
-    /usr/bin/awk -F '\t' '{print $1, $2, $5}' |
-    paste -s -d '\n' |
-    xargs -0 notify-send "Upcoming Appointments"
